575d5d4a046f0ddef61c7cf76a8cafa0a131fe65
Aditya <sp.aditya@gmail.com>
2018-03-15 14:26:54 +0530

    Update mypage.php

--- mypage.php
+++ mypage.php
@@ -6,7 +6,7 @@
  echo "Connected Successfully";
  $res = mysqli_query($conn, "select distinct User from user");
  while ($row = mysqli_fetch_assoc($res)){
-	echo "<h1> " . $row["name"] . "</h1>";
+	echo "<h1> " . $row["User"] . "</h1>";
  }
  echo "Pitching in from " . gethostname();
 
